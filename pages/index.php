153839ad79f04553f9d6dc4693dcf74440b66b24
Weyenriah <weyenriah@gmail.com>
2018-06-20 11:45:47 +0200

    Add changes to news flow and some bibs and bobs

--- pages/index.php
+++ pages/index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="sv">
 
+<!-- Calls for head -->
 <?php include('../components/head.php') ?>
 
 <body>
 
+    <!-- Calls for navigation -->
     <?php include('../components/navigation.php') ?>
 
+    <!-- Specific heading to this page -->
     <header class="header">
         <h1>Välkommen</h1>
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu ex in nulla feugiat sollicitudin. Morbi feugiat facilisis enim quis aliquet. Nunc eu massa sit amet nisl euismod sollicitudin at sit amet est. Fusce vitae rhoncus ante, quis mattis dui. Nunc commodo pellentesque tortor, a ultrices ligula tincidunt quis. Nunc imperdiet sapien id sapien interdum, nec interdum odio convallis. Vestibulum id odio tortor.
         </p>
     </header>
 
+    <!-- Carousel -->
     <section class="carousel-container red-background">
         <div class="slide fade">
             <div class="image"> <img src="../images/dollar.jpg"> </div>
@@ -50,24 +54,59 @@
         <a onclick="changeSlide(1)" class="next"> <img src="../images/arrow.png"> </a>
     </section>
 
-    <section class="newsflow">
-        <h3> Nyheter </h3>
-        <article class="news">
-            <section class="news-text">
-                <h4> 16 Juni, 2018 </h4>
+    <!-- News flow -->
+    <section class="white-background general-grid" id="newsflow">
+        <h2> Nyheter </h2>
+        <article class="white-paragraph" id="news-container">
+            <section class="news-card">
+                <div class="news-img">
+                    <img src="../images/larissa.jpg">
+                </div>
+                <h5 class="second-row-heading"> 16 Juni, 2018 </h5>
                 <p> Idag flyttade Larissa in på katthemmet! Du kan läsa mer om henne här. </p>
             </section>
-            <img class="news-image" src="../images/larissa.jpg">
+            <hr/>
+            <section class="news-card">
+                <h5 class="second-row-heading"> 16 Juni, 2018 </h5>
+                <p> Idag flyttade Larissa in på katthemmet! Du kan läsa mer om henne här. </p>
+            </section>
+            <hr/>
+            <section class="news-card">
+                <div class="news-img">
+                    <img src="../images/larissa.jpg">
+                </div>
+                <h5 class="second-row-heading"> 16 Juni, 2018 </h5>
+                <p> Idag flyttade Larissa in på katthemmet! Du kan läsa mer om henne här. </p>
+            </section>
+            <section class="news-card">
+                <div class="news-img">
+                    <img src="../images/larissa.jpg">
+                </div>
+                <h5 class="second-row-heading"> 16 Juni, 2018 </h5>
+                <p> Idag flyttade Larissa in på katthemmet! Du kan läsa mer om henne här. </p>
+            </section>
+            <section class="news-card">
+                <div class="news-img">
+                    <img src="../images/larissa.jpg">
+                </div>
+                <h5 class="second-row-heading"> 16 Juni, 2018 </h5>
+                <p> Idag flyttade Larissa in på katthemmet! Du kan läsa mer om henne här. </p>
+            </section>
         </article>
+        <div id="hide-show">
+            <button id="my-button" onclick="show()"> Visa mer </button>
+        </div>
     </section>
 
+    <!-- Calls for footer -->
     <?php include('../components/footer.php') ?>
 
 <script>
+    // === CAROUSEL ===
     let slideIndex = 1;
     showSlides(slideIndex);
+    // Changes slide every 3s
     setInterval(() => {
-        // if (!hasMoved){
         showSlides(slideIndex++);
     }, 3000);
 
@@ -80,19 +119,41 @@
         let i;
         let slide = document.getElementsByClassName("slide");
 
+        // Adds display none on every slide to hide it
         for(i = 0; i < slide.length; i++) {
             slide[i].style.display = "none";
         }
 
+        // If slideIndex is greater than slide.length, add 1
         if(slideIndex > slide.length) {
             slideIndex = 1;
         }
+        // If slideIndex is less than 1, slideIndex equals to slide.length
         if(slideIndex < 1) {
             slideIndex = slide.length;
         }
 
         slide[slideIndex - 1].style.display = "block";
     }
+
+    // === HIDE AND SHOW CONTENT ===
+    let button = document.getElementById('hide-show');
+    let container = document.getElementById('news-container');
+    let buttonText = document.getElementById('my-button');
+
+    /* Checks if container contains class expanded, "if" it'll remove the class "else" it'll add it */
+    button.onclick = function show(){
+        if(container.classList.contains("expanded")) {
+            container.classList.remove("expanded");
+            /* Adds text so that the button is correct */
+            buttonText.innerHTML = 'Visa mer';
+        } else {
+            container.classList.add("expanded");
+            /* Adds text so that the button is correct */
+            buttonText.innerHTML = 'Dölj';
+        }
+    }
+
 </script>
 </body>
 </html>
