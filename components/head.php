d897e424804e279c1efcbf5d79a663206a9af199
Weyenriah <weyenriah@gmail.com>
2018-09-09 16:19:26 +0200

    Add responsiveness

--- components/head.php
+++ components/head.php
@@ -2,6 +2,8 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Takes away EDGE browser default design of phone numbers -->
+    <meta name="format-detection" content="telephone=no">
 
     <!-- Fontawesome-required CSS -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
