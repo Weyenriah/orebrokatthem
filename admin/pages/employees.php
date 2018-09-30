4934c2f0ad3aff743efed6fad7d087fc3f8474b3
Weyenriah <weyenriah@gmail.com>
2018-09-30 11:59:44 +0200

    Add employee flow

---
+++ admin/pages/employees.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../components/resources.php';
+
+$employees = $database->getEmployees();
+
+?>
+
+<section id="employees">
+    <div class="employees">
+        <?php
+        foreach ($employees as $employee) {
+            ?>
+            <article class="employee">
+                <?php if ($employee['image'] !== '') { ?>
+                    <div class="employee-img">
+                        <img src="<?php echo(UPLOADS_FOLDER . 'images/' . $employee['image']); ?>">
+                    </div>
+                <?php } ?>
+                <div class="employee-text">
+                    <div class="change-employee">
+                        <a href="#"> <i class="fas fa-pencil-alt"></i> Ändra anställd </a>
+                        <a href="#"> <i class="fas fa-times"></i> Ta bort anställd </a>
+                    </div>
+                    <div class="employee-information">
+                        <h3> <?php echo($employee['name']) ?> </h3>
+                        <small> <?php echo($employee['title']) ?> </small>
+                        <p> <i class="fas fa-phone"></i> <?php echo($employee['telephone']) ?> </p>
+                        <a href="mailto:<?php echo($employee['email']) ?>">
+                            <i class="fas fa-envelope"></i> <?php echo($employee['email']) ?>
+                        </a>
+                    </div>
+                </div>
+            </article>
+        <?php } ?>
+    </div>
+</section>
