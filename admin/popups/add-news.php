7f66b2974f5179285cb84938fe36f31d62417f1e
Weyenriah <weyenriah@gmail.com>
2018-11-21 16:08:55 +0100

    Add 'add news'-popup

--- admin/popups/add-news.php
+++ admin/popups/add-news.php
@@ -1,4 +1,4 @@
-<div class="add-news-container" id="popup-news">
+<div class="add-news-container" id="popup-add-news">
     <article class="add-news">
         <div class="add-header">
             <h2> Lägg till Nyhet </h2>
@@ -7,19 +7,15 @@
 
         <form class="add-form" method="POST" enctype="multipart/form-data">
             <!-- Add picture -->
-            <input type="file" name="cat-image" class="cat-image">
+            <input type="file" name="news-image" class="news-image">
 
             <!-- Add cat information -->
             <div class="info">
-                <label for="catname"> Datum </label>
-                <input type="text" name="catname" id="catname" value="Skriv ett datum (yyyy-mm-dd)">
-            </div>
-            <div class="info">
                 <label for="desc"> Beskrivning </label>
-                <textarea id="desc" rows="6" cols="50"></textarea>
+                <textarea name="desc" id="desc" rows="6" cols="50"></textarea>
             </div>
 
-            <button class="add-button" type="submit" name="add-cat"> Lägg till </button>
+            <button class="add-button" type="submit" name="add-news"> Lägg till </button>
         </form>
     </article>
 </div>
@@ -27,7 +23,7 @@
 <script>
     /* === HIDE POPUP === */
     function hidePopupNews() {
-        let popup = document.getElementById("popup-news");
+        let popup = document.getElementById("popup-add-news");
 
         popup.style.display = "none";
     }
