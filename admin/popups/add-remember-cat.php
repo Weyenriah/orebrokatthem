7373cbd1d4dd0ecc6ea1f7c3e1337d5c6ba23e97
Weyenriah <weyenriah@gmail.com>
2018-11-25 12:20:51 +0100

    Add 'add cat'-popup to remember-flow

--- admin/popups/add-remember-cat.php
+++ admin/popups/add-remember-cat.php
@@ -12,38 +12,38 @@
             <!-- Add cat information -->
             <div class="info">
                 <label for="catname"> Namn </label>
-                <input type="text" name="catname" id="catname" value="Kattens namn...">
+                <input type="text" name="catname" id="catname" placeholder="Kattens namn...">
             </div>
             <div class="life">
                 <div class="remem-info">
                     <label for="born"> Född </label>
-                    <input type="text" name="born" id="born" value="(yyyy)">
+                    <input type="text" name="born" id="born" placeholder="(yyyy)">
                 </div>
                 <div class="remem-info">
                     <label for="died"> Död </label>
-                    <input type="text" name="died" id="died" value="(yyyy-mm-dd)">
+                    <input type="text" name="died" id="died" placeholder="(yyyy-mm-dd)">
                 </div>
             </div>
             <div class="life">
                 <div class="remem-info">
                     <label for="came"> Kom till katthem </label>
-                    <input type="text" name="came" id="came" value="(yyyy-mm-dd)">
+                    <input type="text" name="came" id="came" placeholder="(yyyy-mm-dd)">
                 </div>
                 <div class="remem-info">
                     <label for="adopted"> Adopterad </label>
-                    <input type="text" name="adopted" id="adopted" value="(yyyy-mm-dd)">
+                    <input type="text" name="adopted" id="adopted" placeholder="(yyyy-mm-dd)">
                 </div>
             </div>
             <div class="info">
                 <label for="desc"> Beskrivning </label>
-                <textarea id="desc" rows="6" cols="50"></textarea>
+                <textarea id="desc" name="desc" class="desc" rows="6" cols="50"></textarea>
             </div>
             <div class="info">
                 <label for="cause"> Dödsorsak </label>
-                <input type="text" name="cause" id="cause" value="Text här...">
+                <input type="text" name="cause" id="cause" placeholder="Text här...">
             </div>
 
-            <button class="add-button" type="submit" name="add-cat"> Lägg till </button>
+            <button class="add-button" type="submit" name="add-remember-cat"> Lägg till </button>
         </form>
     </article>
 </div>
