1ec1bed32ca799c408a4c98ba187f9c6876852c0
Weyenriah <weyenriah@gmail.com>
2018-12-05 13:48:42 +0100

    Add php so that admin will be able to change textfields

--- includes/admin/textfield-pages/home.php
+++ includes/admin/textfield-pages/home.php
@@ -1,68 +1,69 @@
+<?php
+
+    $fields = [
+        [
+            'element' => 'home-header',
+            'text' => 'Ändra Header',
+            'rows' => 10
+        ],
+        [
+            'element' => 'found-important',
+            'text' => 'Ändra "Viktig text" för "Har du hittat en katt?',
+            'rows' => 5
+        ],
+        [
+            'element' => 'found-text',
+            'text' => 'Ändra allmän text för "Har du hittat en katt?"',
+            'rows' => 10
+        ],
+        [
+            'element' => 'relocate-important',
+            'text' => 'Ändra "Viktig text" för "Omplacering av katt"',
+            'rows' => 5
+        ],
+        [
+            'element' => 'relocate-text',
+            'text' => 'Ändra allmän text för "Omplacering av katt"',
+            'rows' => 10
+        ],
+        [
+            'element' => 'home-remember',
+            'text' => 'Ändra text i Minneslunden',
+            'rows' => 10
+        ]
+    ];
+
+    foreach ($fields as $field) {
+        if(isset($_POST[$field['element']])) {
+            if(is_string($_POST[$field['element']])) {
+                $data = htmlentities(trim($_POST[$field['element']]));
+
+                $database->changeTextfield($field['element'], $data);
+
+                $goToPage = 'home';
+            }
+        }
+    }
+
+?>
 <section class="textfield page" id="home">
     <div class="textfield-header">
         <h2> Ändra på sida: Hem </h2>
-        <button type="button" id="code-help-home" onclick="commandoHome()"> Kodhjälp </button>
-    </div>
-    <div id="commando-home">
-        <h3> Kortkommandon </h3>
-        <p> &lt;br/&gt; = Enter (2  på rad för nytt stycke) <br/>
-            &lt;i&gt; Text &lt;/i&gt; = <i>Kursiv text</i> <br/>
-            &lt;b&gt; Text &lt;/b&gt; = <b>Fetstilad text</b> </p>
     </div>
     <div class="forms">
-        <form class="form">
-            <div class="text-form">
-                <label for="home-header"> Ändra Header </label>
-                <textarea id="home-header" rows="10" cols="50"><?php echo($database->getContent('home-header')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
+        <?php foreach ($fields as $field) {
+        echo "<form class='form' method='post'>
+            <div class='text-form'>
+                <label for='{$field['element']}'>{$field['text']}</label>
+                <textarea 
+                    id='{$field['element']}' 
+                    name='{$field['element']}' 
+                    rows='{$field['rows']}' 
+                    cols='50'>{$database->getContent($field['element'])}</textarea>
+                <button type='submit' value='Ändra'> Ändra </button>
             </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="found-important"> Ändra "Viktig text" för "Har du hittat en katt?" </label>
-                <textarea id="found-important" rows="5" cols="50"><?php echo($database->getContent('found-important')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="found-text"> Ändra allmän text för "Har du hittat en katt?" </label>
-                <textarea id="found-text" rows="10" cols="50"><?php echo($database->getContent('found-text')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="relocate-important"> Ändra "Viktig text" för "Omplacering av katt" </label>
-                <textarea id="relocate-important" rows="5" cols="50"><?php echo($database->getContent('relocate-important')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="relocate-text"> Ändra allmän text för "Omplacering av katt" </label>
-                <textarea id="relocate-text" rows="10" cols="50"><?php echo($database->getContent('relocate-text')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="home-remember"> Ändra text i Minneslunden </label>
-                <textarea id="home-remember" rows="10" cols="50"><?php echo($database->getContent('home-remember')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
+        </form>";
+        } ?>
     </div>
 </section>
 
