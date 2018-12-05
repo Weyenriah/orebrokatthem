98c6f3e787e243ff7b8330983aca7b9f12e803de
Weyenriah <weyenriah@gmail.com>
2018-12-05 15:45:17 +0100

    Add php so that admin will be able to change textfields

--- includes/admin/textfield-pages/support.php
+++ includes/admin/textfield-pages/support.php
@@ -1,90 +1,125 @@
+<?php
+// All the changeable fields
+$fields = [
+    [
+        'element' => 'support-header',
+        'text' => 'Ändra Header',
+        'rows' => 10
+    ],
+    [
+        'element' => 'support-member',
+        'text' => 'Ändra "Bli Medlem"',
+        'rows' => 10
+    ],
+    [
+        'element' => 'support-catneed',
+        'text' => 'Ändra önskelista för katter',
+        'rows' => 10
+    ],
+    [
+        'element' => 'support-humanneed',
+        'text' => 'Ändra önskelista för personalen',
+        'rows' => 10
+    ],
+    [
+        'text' => 'Ändra kontaktinformation för "Anmälning"',
+        'fields' => [
+            [
+                'element' => 'support-insurance-name',
+                'text' => 'Namn',
+                'rows' => 1
+            ],
+            [
+                'element' => 'support-insurance-email',
+                'text' => 'E-post',
+                'rows' => 1
+            ],
+            [
+                'element' => 'support-insurance-tele',
+                'text' => 'Telefonnummer',
+                'rows' => 1
+            ],
+            [
+                'element' => 'support-insurance-availab',
+                'text' => 'Tillgänglighet',
+                'rows' => 1
+            ]
+        ]
+    ]
+];
+
+// Change fields
+foreach ($fields as $field) {
+    if (isset($field['fields'])){
+        foreach ($field['fields'] as $f) {
+            if(isset($_POST[$f['element']])) {
+                if(is_string($_POST[$f['element']])) {
+                    $data = htmlentities(trim($_POST[$f['element']]));
+
+                    $database->changeTextfield($f['element'], $data);
+
+                    $goToPage = 'support';
+                }
+            }
+        }
+    } else {
+        if(isset($_POST[$field['element']])) {
+            if(is_string($_POST[$field['element']])) {
+                $data = htmlentities(trim($_POST[$field['element']]));
+
+                $database->changeTextfield($field['element'], $data);
+
+                $goToPage = 'support';
+            }
+        }
+    }
+}
+?>
+
 <section class="textfield page" id="support">
     <div class="textfield-header">
         <h2> Ändra på sida: Stöd Oss </h2>
-        <button type="button" id="code-help-support" onclick="commandoSupport()"> Kodhjälp </button>
-    </div>
-    <div id="commando-support">
-        <h3> Kortkommandon </h3>
-        <p> &lt;br/&gt; = Enter (2  på rad för nytt stycke) <br/>
-            &lt;i&gt; Text &lt;/i&gt; = <i>Kursiv text</i> <br/>
-            &lt;b&gt; Text &lt;/b&gt; = <b>Fetstilad text</b> <br/>
-            &lt;li&gt; Text &lt;/li&gt; = Ny punkt i lista <br/>
-            &lt;small&gt; Text &lt;/small&gt; = <small> Mindre text </small> <br/> </p>
     </div>
     <div class="forms">
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra Header </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('support-header')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra "Bli Medlem" </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('support-member')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-    </div>
-    </form>
-    </div>
-
-    <div class="forms">
-        <form class="form">
-            <div class="text-form multiple">
-                <h3> Ändra kontaktinformation för "Anmälning" </h3>
-                <label for="name"> Namn </label>
-                <input type="text" id="name" value="<?php echo($database->getContent('support-insurance-name')); ?>">
-
-                <label for="email"> E-post </label>
-                <input type="text" id="email" value="<?php echo($database->getContent('support-insurance-email')); ?>">
-
-                <label for="tele"> Telefonnummer </label>
-                <input type="text" id="tele" value="<?php echo($database->getContent('support-insurance-tele')); ?>">
-
-                <label for="availab"> Tillgänglighet </label>
-                <input type="text" id="availab" value="<?php echo($database->getContent('support-insurance-availab')); ?>">
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-    </div>
-
-    <div class="forms">
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra önskelista för katter </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('support-catneed')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra önskelista för personalen </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('support-humanneed')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
+        <?php
+        // First check if fields exist, then do multiple fields, if there's none: One field.
+        foreach ($fields as $field) {
+            if (isset($field['fields'])) {
+                echo "
+                    <form class='form' method='post'>
+                        <div class='text-form multiple'>
+                            <h3>{$field['text']}</h3>
+                ";
+                foreach ($field['fields'] as $f) {
+                    echo "
+                        <label for='{$f['element']}'>{$f['text']}</label>
+                        <textarea 
+                            id='{$f['element']}' 
+                            name='{$f['element']}' 
+                            rows='{$f['rows']}' 
+                            cols='50'>{$database->getContent($f['element'])}</textarea>
+                    ";
+                }
+                echo "
+                        <button type='submit' value='Ändra'> Ändra </button>
+                    </div>
+                </form>
+                ";
+            } else{
+                echo "
+                    <form class='form' method='post'>
+                        <div class='text-form'>
+                            <label for='{$field['element']}'>{$field['text']}</label>
+                            <textarea 
+                                id='{$field['element']}' 
+                                name='{$field['element']}' 
+                                rows='{$field['rows']}' 
+                                cols='50'>{$database->getContent($field['element'])}</textarea>
+                            <button type='submit' value='Ändra'> Ändra </button>
+                        </div>
+                    </form>
+                ";
+            }
+        } ?>
     </div>
 </section>
-
-<script>
-    /* === SHOW AND HIDE CODE HELP === */
-    function commandoSupport() {
-        let commando = document.getElementById('commando-support');
-        let button = document.getElementsByClassName('code-help-support');
-
-        if (commando.classList.contains('display-support')) {
-            commando.classList.remove('display-support');
-            button.classList.remove('active-button-support');
-        } else {
-            commando.classList.add('display-support');
-            button.classList.add('active-button-support');
-        }
-    }
-</script>
