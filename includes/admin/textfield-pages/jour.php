96701b4e0c6bd418202862c745633a1d9f1f5fa6
Weyenriah <weyenriah@gmail.com>
2018-12-05 15:35:34 +0100

    Add php so that admin will be able to change textfields

--- includes/admin/textfield-pages/jour.php
+++ includes/admin/textfield-pages/jour.php
@@ -1,74 +1,113 @@
+<?php
+
+
+// All the changeable fields
+$fields = [
+    [
+        'element' => 'jour-header',
+        'text' => 'Ändra Header',
+        'rows' => 10
+    ],
+    [
+        'element' => 'jour-how',
+        'text' => 'Ändra "Hur är det att vara jourhem?"',
+        'rows' => 10
+    ],
+    [
+        'element' => 'jour-tips',
+        'text' => 'Ändra "Tips för dig med skygg jourhemskatt"',
+        'rows' => 10
+    ],
+    [
+        'text' => 'Ändra kontaktinformation för "Anmälning"',
+        'fields' => [
+            [
+                'element' => 'jour-contact-name',
+                'text' => 'Namn',
+                'rows' => 1
+            ],
+            [
+                'element' => 'jour-contact-tele',
+                'text' => 'Telefonnummer',
+                'rows' => 1
+            ]
+        ]
+    ]
+];
+
+// Change fields
+foreach ($fields as $field) {
+    if (isset($field['fields'])){
+        foreach ($field['fields'] as $f) {
+            if(isset($_POST[$f['element']])) {
+                if(is_string($_POST[$f['element']])) {
+                    $data = htmlentities(trim($_POST[$f['element']]));
+
+                    $database->changeTextfield($f['element'], $data);
+
+                    $goToPage = 'jour';
+                }
+            }
+        }
+    } else {
+        if(isset($_POST[$field['element']])) {
+            if(is_string($_POST[$field['element']])) {
+                $data = htmlentities(trim($_POST[$field['element']]));
+
+                $database->changeTextfield($field['element'], $data);
+
+                $goToPage = 'jour';
+            }
+        }
+    }
+}
+
+?>
+
 <section class="textfield page" id="jour">
     <div class="textfield-header">
         <h2> Ändra på sida: Bli Jourhem </h2>
-        <button type="button" id="code-help-jour" onclick="commandoJour()"> Kodhjälp </button>
-    </div>
-    <div id="commando-jour">
-        <h3> Kortkommandon </h3>
-        <p> &lt;br/&gt; = Enter (2  på rad för nytt stycke) <br/>
-            &lt;i&gt; Text &lt;/i&gt; = <i>Kursiv text</i> <br/>
-            &lt;b&gt; Text &lt;/b&gt; = <b>Fetstilad text</b> <br/>
-            &lt;li&gt; Text &lt;/li&gt; = Ny punkt i lista <br/>
-            &lt;h5&gt; Text &lt;/h5&gt; = Rubrik <br/>
-            &lt;p&gt; Text &lt;/p&gt; = Text efter rubrik </p>
     </div>
     <div class="forms">
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra Header </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('jour-header')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra "Hur är det att vara jourhem?" </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('jour-how')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
-    </div>
-
-    <div class="form">
-        <div class="text-form">
-            <label for="text"> Ändra "Tips för dig med skygg jourhemskatt" </label>
-            <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('jour-tips')); ?></textarea>
-
-            <button type="submit" value="Ändra"> Ändra </button>
-        </div>
-    </div>
-
-    <div class="forms">
-        <form class="form">
-            <div class="text-form multiple">
-                <h3> Ändra kontaktinformation för "Anmälning" </h3>
-                <label for="name"> Namn </label>
-                <input type="text" id="name" value="<?php echo($database->getContent('jour-contact-name')); ?>">
-
-                <label for="tele"> Telefonnummer </label>
-                <input type="text" id="tele" value="<?php echo($database->getContent('jour-contact-tele')); ?>">
-
-                <button type="submit" value="Ändra"> Ändra </button>
-            </div>
-        </form>
+        <?php
+        // First check if fields exist, then do multiple fields, if there's none: One field.
+        foreach ($fields as $field) {
+            if (isset($field['fields'])) {
+                echo "
+                    <form class='form' method='post'>
+                        <div class='text-form multiple'>
+                            <h3>{$field['text']}</h3>
+                ";
+                foreach ($field['fields'] as $f) {
+                    echo "
+                        <label for='{$f['element']}'>{$f['text']}</label>
+                        <textarea 
+                            id='{$f['element']}' 
+                            name='{$f['element']}' 
+                            rows='{$f['rows']}' 
+                            cols='50'>{$database->getContent($f['element'])}</textarea>
+                    ";
+                }
+                echo "
+                        <button type='submit' value='Ändra'> Ändra </button>
+                    </div>
+                </form>
+                ";
+            } else{
+                echo "
+                    <form class='form' method='post'>
+                        <div class='text-form'>
+                            <label for='{$field['element']}'>{$field['text']}</label>
+                            <textarea 
+                                id='{$field['element']}' 
+                                name='{$field['element']}' 
+                                rows='{$field['rows']}' 
+                                cols='50'>{$database->getContent($field['element'])}</textarea>
+                            <button type='submit' value='Ändra'> Ändra </button>
+                        </div>
+                    </form>
+                ";
+            }
+        } ?>
     </div>
 </section>
-
-<script>
-    /* === SHOW AND HIDE CODE HELP === */
-    function commandoJour() {
-        let commando = document.getElementById('commando-jour');
-        let button = document.getElementsByClassName('code-help-jour');
-
-        if (commando.classList.contains('display-jour')) {
-            commando.classList.remove('display-jour');
-            button.classList.remove('active-button-jour');
-        } else {
-            commando.classList.add('display-jour');
-            button.classList.add('active-button-jour');
-        }
-    }
-</script>
