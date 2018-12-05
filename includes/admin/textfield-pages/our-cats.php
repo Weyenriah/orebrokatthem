46c50e5abee074b42e147ebcaef32f4b2ae28125
Weyenriah <weyenriah@gmail.com>
2018-12-05 15:39:11 +0100

    Add php so that admin will be able to change textfields

--- includes/admin/textfield-pages/our-cats.php
+++ includes/admin/textfield-pages/our-cats.php
@@ -1,38 +1,44 @@
+<?php
+// All the changeable fields
+$fields = [
+    [
+        'element' => 'ourcats-header',
+        'text' => 'Ändra Header',
+        'rows' => 10
+    ]
+];
+
+// Change fields
+foreach ($fields as $field) {
+    if(isset($_POST[$field['element']])) {
+        if(is_string($_POST[$field['element']])) {
+            $data = htmlentities(trim($_POST[$field['element']]));
+
+            $database->changeTextfield($field['element'], $data);
+
+            $goToPage = 'our-cats';
+        }
+    }
+}
+?>
+
 <section class="textfield page" id="our-cats">
     <div class="textfield-header">
         <h2> Ändra på sida: Våra Katter </h2>
-        <button type="button" id="code-help-cats" onclick="commandoCats()"> Kodhjälp </button>
-    </div>
-    <div id="commando-cats">
-        <h3> Kortkommandon </h3>
-        <p> &lt;br/&gt; = Enter (2  på rad för nytt stycke) <br/>
-            &lt;i&gt; Text &lt;/i&gt; = <i>Kursiv text</i> <br/>
-            &lt;b&gt; Text &lt;/b&gt; = <b>Fetstilad text</b> </p>
     </div>
     <div class="forms">
-        <form class="form">
-            <div class="text-form">
-                <label for="text"> Ändra Header </label>
-                <textarea id="text" rows="10" cols="50"><?php echo($database->getContent('ourcats-header')); ?></textarea>
-
-                <button type="submit" value="Ändra"> Ändra </button>
+        <?php foreach ($fields as $field) {
+            echo "<form class='form' method='post'>
+            <div class='text-form'>
+                <label for='{$field['element']}'>{$field['text']}</label>
+                <textarea 
+                    id='{$field['element']}' 
+                    name='{$field['element']}' 
+                    rows='{$field['rows']}' 
+                    cols='50'>{$database->getContent($field['element'])}</textarea>
+                <button type='submit' value='Ändra'> Ändra </button>
             </div>
-        </form>
+        </form>";
+        } ?>
     </div>
 </section>
-
-<script>
-    /* === SHOW AND HIDE CODE HELP === */
-    function commandoCats() {
-        let commando = document.getElementById('commando-cats');
-        let button = document.getElementsByClassName('code-help-cats');
-
-        if (commando.classList.contains('display-cats')) {
-            commando.classList.remove('display-cats');
-            button.classList.remove('active-button-cats');
-        } else {
-            commando.classList.add('display-cats');
-            button.classList.add('active-button-cats');
-        }
-    }
-</script>
