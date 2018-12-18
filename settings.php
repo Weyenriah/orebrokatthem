dbfa3e9367ae8d5fa3c7bc2571859c595f2f98dc
Weyenriah <weyenriah@gmail.com>
2018-12-18 16:18:51 +0100

    Add settings.php

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+define("BASE_URL", (isset($_ENV['BASE_URL'])) ? $_ENV['BASE_URL'] : "/orebrokatthem/public/");
+
+// Define constants for database connection
+define('DB_HOST', (isset($_ENV['DB_HOST'])) ? $_ENV['DB_HOST'] : 'localhost');
+define('DB_NAME', (isset($_ENV['DB_NAME'])) ? $_ENV['DB_NAME'] : 'okh');
+define('DB_USER', (isset($_ENV['DB_USER'])) ? $_ENV['DB_USER'] : 'root');
+define('DB_PASSWORD', (isset($_ENV['DB_PASSWORD'])) ? $_ENV['DB_PASSWORD'] : '');
+
+define('UPLOADS_FOLDER', (isset($_ENV['UPLOADS_FOLDER'])) ? $_ENV['UPLOADS_FOLDER'] : 'storage' . DIRECTORY_SEPARATOR );
+
+define('PASSWORD_SALT', (isset($_ENV['PASSWORD_SALT'])) ? $_ENV['PASSWORD_SALT'] : '');
