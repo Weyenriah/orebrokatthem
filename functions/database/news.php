ba374015a641728a18094c69b1a68a73aac75c9c
Weyenriah <weyenriah@gmail.com>
2019-01-02 21:56:30 +0100

    Add functionality so that you can add images and change them

--- functions/database/news.php
+++ functions/database/news.php
@@ -2,30 +2,47 @@
 
 trait News {
     // Add News
-    public function addNews($news) {
+    public function addNews($news, $image) {
         if ($this->changesLastHour('news') > 20) {
             return false;
         } else {
-            $sql = 'INSERT INTO news (news) VALUES (:news)';
+            $sql = 'INSERT INTO news (
+                      `news`,
+                      `image`
+                    ) VALUES (
+                      :news,
+                      :image
+                    )';
             // Prepares a query
             $stmt = $this->pdo->prepare($sql);
             // Sends query to database
             return $stmt->execute(array(
                 'news' => $news,
+                'image' => $image,
             ));
         }
     }
 
     // Change News
-    public function changeNews($id, $news) {
-        $sql = 'UPDATE news SET news = :news WHERE id = :id';
+    public function changeNews($id, $news, $image) {
+        $sql = 'UPDATE news SET news = :news';
+
+        $parameters = [
+            'news' => $news,
+            'id' => $id,
+        ];
+
+        if ($image != null) {
+            $sql .= ', image = :image';
+            $parameters['image'] = $image;
+        }
+
+        $sql .= ' WHERE id = :id';
+
         // Prepares a query
         $stmt = $this->pdo->prepare($sql);
         // Sends query to database
-        return $stmt->execute(array(
-            'id' => $id,
-            'news' => $news,
-        ));
+        return $stmt->execute($parameters);
     }
 
     // Delete News
--- includes/admin/pages/news.php
+++ includes/admin/pages/news.php
@@ -10,9 +10,12 @@ if (isset($_POST['removeNewsPost'])) {
 // Add news
 if(isset($_POST['add-news'])) {
     $desc = htmlentities(trim($_POST['desc']));
+    $file = isset($_FILES['news-image']) ? $_FILES['news-image'] : null;
 
     if(is_string($desc) == true && $desc !== '' && strlen($desc) < 10000) {
-        $newsAdded = $database->addNews($desc);
+        $image = SaveFile($file);
+
+        $newsAdded = $database->addNews($desc, $image);
 
         $goToPage = 'news';
     } else {
@@ -26,9 +29,12 @@ if(isset($_POST['add-news'])) {
 if(isset($_POST['change-news'])) {
     $id = $_POST['id'];
     $desc = htmlentities(trim($_POST['desc']));
+    $file = isset($_FILES['news-image']) ? $_FILES['news-image'] : null;
 
     if(is_string($desc) == true && $desc !== '' && strlen($desc) < 10000) {
-        $newsAdded = $database->changeNews($id, $desc);
+        $image = SaveFile($file);
+
+        $newsAdded = $database->changeNews($id, $desc, $image);
 
         $goToPage = 'news';
     } else {
