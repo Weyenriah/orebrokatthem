c86a0920ace381d33a8a912be30a007b36ca763b
Weyenriah <weyenriah@gmail.com>
2019-01-04 20:12:08 +0100

    Fixed file paths again

--- includes/head.php
+++ includes/head.php
@@ -9,23 +9,23 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
     <!-- General styling CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/css/general.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/general.css">
 
     <!-- Specific CSS for pages -->
-    <link rel="stylesheet" type="text/css" href="assets/css/pages/home.css"> <!-- Homepage -->
-    <link rel="stylesheet" type="text/css" href="assets/css/pages/adopt.css"> <!-- Adopt-page -->
-    <link rel="stylesheet" type="text/css" href="assets/css/pages/our-cats.css"> <!-- Cat-page popup -->
-    <link rel="stylesheet" type="text/css" href="assets/css/pages/jour.css"> <!-- Emergency home-page -->
-    <link rel="stylesheet" type="text/css" href="assets/css/pages/about.css"> <!-- About Us-page -->
-    <link rel="stylesheet" type="text/css" href="assets/css/pages/support.css"> <!-- Support Us-page -->
-    <link rel="stylesheet" type="text/css" href="assets/css/popups/cat-page.css"> <!-- Cat-page popup -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/pages/home.css"> <!-- Homepage -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/pages/adopt.css"> <!-- Adopt-page -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/pages/our-cats.css"> <!-- Cat-page popup -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/pages/jour.css"> <!-- Emergency home-page -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/pages/about.css"> <!-- About Us-page -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/pages/support.css"> <!-- Support Us-page -->
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/popups/cat-page.css"> <!-- Cat-page popup -->
 
     <!-- Components CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/css/components/footer.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/components/navigation.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/components/footer.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/components/navigation.css">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="<?php echo BASE_URL?>assets/images/favicon.png"/>
 
     <!-- Webpage title -->
     <title>Örebro Katthem</title>
